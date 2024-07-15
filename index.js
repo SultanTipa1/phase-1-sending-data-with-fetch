3d5ea76a8a0ae6ebbef33f6017d16a9a1e088fe7
SultanTipa1 <josephngumbaugush@gmail.com>
2024-07-15 03:40:42 +0300

    Adds all files

--- index.js
+++ index.js
@@ -1,1 +1,78 @@
 // Add your code here
+fetch(destinationURL, configarationObject);
+
+const formData = {
+    dogName: "Byron",
+    dogreed: "poodle",
+}
+
+const configarationObject = {
+    method: "POST",
+    header: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify(formData),
+};
+
+fetch("http://localhost:3000/dogs", configurationObject)
+.then(function (response){
+    return response.json();
+})
+.then(function (object){
+    console.log(object);
+});
+
+fetch("http://localhost:3000/dogs", {
+    method: "POST",
+    headers;{
+        "Content-Type": "application/json",
+        'Accept': "application/json",
+    }
+    body: JSON.stringify({
+        dogName: "Byron",
+        dogBreed: "poodle",
+    }),
+});
+
+
+
+const destinationURL = "http://localhost:3000/users";  //server
+
+fetch(destinationURL, configarationObject);
+
+const formData ={
+    userName: "name",
+    userEmail:"email",
+}
+
+const configurationObject = {
+    method: "POST",
+    headers;{
+        "Content-Type": "application/json",
+        'Accept': "application/json",
+    }
+    body: JSON.stringify(formData),
+};
+
+fetch("http://localhost:3000/users", configurationObject)
+.then(function (response){
+    return response.json();
+})
+.then(function (object){
+    console.log(object);
+    appendToDom(object);
+    .catch(function (error) {
+        console.error("Error fetching data:", error);
+        appendToDom(object);
+});
+
+function appendToDom(data){
+    const valueToAppend = data.someValue;
+    const container = document.getElementById("result-container"); 
+
+  const newElement = document.createElement("div");
+  newElement.textContent = valueToAppend;
+
+  container.appendChild(newElement);
+}
